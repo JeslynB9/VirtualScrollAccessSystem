da87afc6f98c96c88e6c42967a66ad1fb7377a66
dhor0241 <destinee.horvath@gmail.com>
2024-10-11 18:24:00 +1100

    implemented filtering scroll functions

--- app/src/main/java/ScrollSystem/FileHandlers/FilterScroll.java
+++ app/src/main/java/ScrollSystem/FileHandlers/FilterScroll.java
@@ -11,11 +11,18 @@ public class FilterScroll {
     private int currentLine;
     private int totalLines;
 
-    public FilterScroll(String path) throws IOException {
+    public FilterScroll(String path) {
         this.path = path;
-        this.reader = new BufferedReader(new InputStreamReader(new FileInputStream(path))); // to read binary file 
-        this.currentLine = -1; 
-        this.totalLines = countLines(); 
+        try {
+            this.reader = new BufferedReader(new InputStreamReader(new FileInputStream(path))); // to read binary file 
+            this.currentLine = -1; 
+            this.totalLines = countLines(); 
+        } catch (IOException e) {
+            System.err.println("Error initialising FilterScroll: " + e.getMessage());
+            e.printStackTrace(); 
+            this.reader = null; 
+            this.totalLines = 0; 
+        }
     }
 
     /**
@@ -25,17 +32,22 @@ public class FilterScroll {
      * @ret: 
      *      string on that line 
      */
-    public String getLine(int lineNumber) throws IOException {
+    public String getLine(int lineNumber) {
         resetReader();
         String line;
-        int currentLine = 0;
-
-        while ((line = reader.readLine()) != null) {
-            if (currentLine == lineNumber) {
-                this.currentLine = currentLine; 
-                return line; 
+        int currentLine = 1;
+    
+        try {
+            while ((line = reader.readLine()) != null) {
+                if (currentLine == lineNumber) {
+                    this.currentLine = currentLine; 
+                    return line; 
+                }
+                currentLine++;
             }
-            currentLine++;
+        } catch (IOException e) {
+            System.err.println("Error reading line: " + lineNumber + " | " + e.getMessage());
+            return null; 
         }
         return null; 
     }
@@ -45,11 +57,17 @@ public class FilterScroll {
      * @ret:
      *      string of next line 
      */
-    public String nextLine() throws IOException {
-        if (currentLine + 1 >= totalLines) {
-            return "END OF FILE"; 
+    public String nextLine() {
+        try{
+            if (currentLine + 1 >= totalLines) {
+                return "END OF FILE"; 
+            }
+            return getLine(currentLine + 1); 
         }
-        return getLine(currentLine + 1); 
+        catch (Exception e) {
+            System.err.println("Error getting next line: " + e.getMessage());
+            return "ERROR";
+        }
     }
 
     /**
@@ -57,27 +75,99 @@ public class FilterScroll {
      * @ret:
      *      string of previous line 
      */
-    public String previousLine() throws IOException {
-        return getLine(currentLine - 1); 
+    public String previousLine() {
+        try {
+            if (currentLine <= 0) {
+                System.out.println("Error: No previous line");
+                return "ERROR";
+            }
+            return getLine(currentLine - 1);
+        } catch (Exception e) {
+            System.err.println("Error getting previous line: " + e.getMessage());
+            return "ERROR";
+        }
+    }
+
+    /**
+     * Gets all the lines in the file 
+     * @ret:
+     *      String of liens in the file 
+     */
+    public String getAllLines() {
+        int totalLines = countLines();
+        StringBuilder result = new StringBuilder();
+    
+        for (int i = 0; i < totalLines; i++) {
+            String line = getLine(i);
+            if (line != null) {
+                result.append(line).append("\n");
+            } 
+            else {
+                System.out.println("Error: Unable to retrieve line " + i);
+                return "ERROR";
+            }
+        }
+    
+        return result.toString();
+    }
+
+    /**
+     * Gets the lines between two given line numbers (inclusive)
+     * @params: 
+     *      start, end : int 
+     * @ret
+     *      lines between the line numbers 
+     */
+    public String getLinesBetween(int start, int end) {
+        if (start < 0 || end < 0 || start >= totalLines || end >= totalLines) {
+            System.out.println("Error: invlid line numbers");
+            return "ERROR";
+        }
+        if (start > end) {
+            System.out.println("Error: end line is larger than start line");
+            return "ERROR";
+        }
+    
+        StringBuilder result = new StringBuilder();
+    
+        for (int i = start - 1; i < end; i++) {
+            String line = getLine(i);
+            if (line != null) {
+                result.append(line).append("\n");
+            } else {
+                System.out.println("ERROR: Unable to retrieve line " + i);
+                return "ERROR";
+            }
+        }
+    
+        return result.toString();
     }
 
     /**
      * Resets the current line 
      */
-    private void resetReader() throws IOException {
-        if (reader != null) {
-            reader.close();
+    private void resetReader() {
+        try {
+            if (reader != null) {
+                reader.close();
+            }
+            this.reader = new BufferedReader(new InputStreamReader(new FileInputStream(path)));
+            this.currentLine = -1; 
+        } catch (IOException e) {
+            System.err.println("Error resetting reader: " + e.getMessage());
         }
-        this.reader = new BufferedReader(new InputStreamReader(new FileInputStream(path)));
-        this.currentLine = -1; 
     }
 
     /** 
      * closes the reader 
      */
-    public void close() throws IOException {
-        if (reader != null) {
-            reader.close();
+    public void close() {
+        try {
+            if (reader != null) {
+                reader.close();
+            }
+        } catch (IOException e) {
+            System.err.println("Error closing reader: " + e.getMessage());
         }
     }
 
@@ -86,13 +176,17 @@ public class FilterScroll {
      * @ret: 
      *      number of lines in the file 
      */
-    private int countLines() throws IOException {
+    public int countLines() {
         int lines = 0;
         try (BufferedReader tempReader = new BufferedReader(new InputStreamReader(new FileInputStream(path)))) {
             while (tempReader.readLine() != null) {
                 lines++;
             }
+
+            return lines;
+        } catch (IOException e) {
+            System.err.println("Error counting lines: " + e.getMessage());
+            return 0;
         }
-        return lines;
     }
 }
