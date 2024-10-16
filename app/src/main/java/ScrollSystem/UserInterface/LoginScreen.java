e9019b942c6c2d27f55ece225ad9c5eefe6fb500
Irene <mche0758@uni.sydney.edu.au>
2024-10-16 15:42:23 +1100

    fix filter for user

--- app/src/main/java/ScrollSystem/UserInterface/LoginScreen.java
+++ app/src/main/java/ScrollSystem/UserInterface/LoginScreen.java
@@ -28,13 +28,13 @@ public class LoginScreen {
     String enteredUsername = "";
     String enteredPassword = "";
 
-    public LoginScreen(PApplet parent, ScrollDatabase scrollDatabase) {
+    public LoginScreen(PApplet parent, ScrollDatabase scrollDatabase, UploadScroll uploadScroll) {
 
         this.parent = parent;
         this.user = new User();
         registerScreen = new RegisterScreen(parent, this);
         viewScrollsGuest = new ViewScrollsGuest(parent);
-        viewScrollsUsers = new ViewScrollsUsers(parent, this);
+        viewScrollsUsers = new ViewScrollsUsers(parent, this, uploadScroll);
         viewScrollsAdmin = new ViewScrollsAdmin(parent, this, scrollDatabase);
         System.out.println("Register initialized");
     }
